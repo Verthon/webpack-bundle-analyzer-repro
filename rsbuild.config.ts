060030954ad7ee38c33df602c60733ab2f7bb203
Krzysztof Sordyl <krzysztof.sordyl@tsh.io>
2026-06-16 07:34:58 +0200

    chore: add rsdoctor repro

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import { RsdoctorRspackPlugin } from '@rsdoctor/rspack-plugin';
 
 const analyze = process.env.ANALYZE;
 const analyzerMode = analyze ? "static" : "json";
@@ -20,6 +21,10 @@ export default defineConfig({
           reportFilename:
             analyzerMode === "json" ? "report.json" : "report.html",
         }),
+        process.env.RSDOCTOR === "true" &&
+          new RsdoctorRspackPlugin({
+            // plugin options
+          }),
       ],
     },
   },
